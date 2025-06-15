5fc7c23f3b025d8485daaeeb4c4b80e051b0582d
JeetDas5 <jeet15083011@gmail.com>
2025-06-15 11:10:23 +0530

    implemented create component server action

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,7 +1,13 @@
 import CompanionForm from "@/components/CompanionForm";
 import React from "react";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const newCompanion = () => {
+const newCompanion = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/sign-in");
+  }
   return (
     <main className="min-lg:w-[40vw] min-mid:w-[70vw] items-center justify-center pt-0">
       <article className="w-full gap-4 flex flex-col">
--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -22,6 +22,8 @@ import {
 } from "@/components/ui/form";
 import { subjects } from "@/constants";
 import { Textarea } from "./ui/textarea";
+import { createCompanion } from "@/lib/actions/companion.actions";
+import { redirect } from "next/navigation";
 
 const formSchema = z.object({
   name: z.string().min(1, { message: "Companion is required" }),
@@ -45,8 +47,16 @@ const CompanionForm = () => {
     },
   });
 
-  const onSubmit = (values: z.infer<typeof formSchema>) => {
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
     console.log(values);
+    const companion = await createCompanion(values);
+
+    if (companion) {
+      redirect(`/companions/${companion.id}`);
+    } else {
+      console.log("Failed to create companion");
+      redirect("/");
+    }
   };
   return (
     <Form {...form}>
@@ -163,7 +173,7 @@ const CompanionForm = () => {
                   </SelectTrigger>
                   <SelectContent>
                     <SelectItem value="formal">Formal</SelectItem>
-                    <SelectItem value="casual">casual</SelectItem>
+                    <SelectItem value="casual">Casual</SelectItem>
                   </SelectContent>
                 </Select>
               </FormControl>
